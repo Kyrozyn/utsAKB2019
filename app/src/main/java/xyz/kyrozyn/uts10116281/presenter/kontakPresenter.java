30c8841a18cf8af3c8958f1a5c2d6c4ed66a24e2
Kyrozyn <arizkirizaldi@email.unikom.ac.id>
2019-05-23 18:25:13 +0700

    cleanup

--- app/src/main/java/xyz/kyrozyn/uts10116281/presenter/kontakPresenter.java
+++ app/src/main/java/xyz/kyrozyn/uts10116281/presenter/kontakPresenter.java
@@ -1,11 +1,5 @@
 package xyz.kyrozyn.uts10116281.presenter;
 
-import android.widget.ArrayAdapter;
-
-import xyz.kyrozyn.uts10116281.R;
-import xyz.kyrozyn.uts10116281.model.Teman;
-import xyz.kyrozyn.uts10116281.model.listTeman;
-
 public class kontakPresenter {
 
     public kontakPresenter(){
